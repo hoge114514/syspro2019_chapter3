0ad01644168cef3343a6b481ab1599946d795fa2
hoge114514 <tatsugi9682@gmail.com>
2019-09-26 02:31:02 +0100

    LEDを一秒周期で点滅させます

--- ledon.py
+++ ledon.py
@@ -1,10 +1,15 @@
 ﻿import time
 import RPi.GPIO as GPIO
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
 
-GPIO.output(14, GPIO.HIGH)
-time.sleep(10)
+
+while (True) :
+    GPIO.output(14, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(14, GPIO.LOW)
+    time.sleep(0.5)
 
 GPIO.cleanup()
